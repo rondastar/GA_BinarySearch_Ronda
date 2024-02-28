d6ab921f67b0ab693bc8d2ea30390a6883e972a8
rondastar <116147793+rondastar@users.noreply.github.com>
2024-02-28 14:20:12 -0800

    binary search methods completed

---
+++ BinarySearch.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_BinarySearch_Ronda
+{
+    internal class BinarySearch
+    {
+        // This method takes an array of integers array and the target integer target as parameters.
+        // It returns an integer representing the index of the target element in the array,
+        // or -1 if the target element is not found.
+        public static int IterativeBinarySearch(int[] array, int target)
+        {
+            // left reference starts at the first index of the array
+            int left = 0;
+
+            // right reference starts at the last index of the array
+            int right = array.Length - 1; 
+
+            // base case - check if left pointer is less than or equal to the right pointer
+            while(left <= right)
+            {   
+                // find mid point of the portion of the array being searched
+                int mid = left + (right - left) / 2;
+
+                // if the target is found at the mid point, return the index of the mid point
+                if (array[mid] == target) return mid;
+
+                // if the value at the mid point is less than the search target,
+                // make the index after the mid point the new left reference.
+                // The next iteration will only search the right half.
+                else if (array[mid] < target) left = mid + 1;
+
+                // if the value at the mid point is more than the search target,
+                // make the index before the mid point the new right reference.
+                // The next iteration will only search the left half.
+                else right = mid - 1;
+
+            } // base case - if left pointer exceeds right pointer, the target was not found
+
+            // return -1 if the search target is not found
+            return -1;
+        } // IterativeBinarySearch
+
+        public static int RecursiveBinarySearch(int[] array, int target, int left, int right)
+        {
+            // base case - check if left pointer is less than or equal to the right pointer
+            while (left <= right)
+            {
+                // find mid point of the portion of the array being searched
+                int mid = left + (right - left) / 2;
+
+                // if the target is found at the mid point, return the index of the mid point
+                if (array[mid] == target) return mid;
+
+                // if the value at the mid point is less than the search target,
+                // search the right half recursively
+                else if (array[mid] < target)
+                {
+                    return RecursiveBinarySearch(array, target, mid + 1, right);
+                }
+
+                // if the value at the mid point is more than the search target,
+                // search the left half recursively
+                else
+                {
+                    return RecursiveBinarySearch(array, target, left, mid - 1);
+                }
+
+            } // base case - if left pointer exceeds right pointer, the target was not found
+
+            // return -1 if the search target is not found
+            return -1;
+        } // IterativeBinarySearch
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,50 @@
-﻿namespace GA_BinarySearch_Ronda
+﻿using System.Diagnostics;
+
+namespace GA_BinarySearch_Ronda
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Test iterative binary search
+            TestIterativeBinarySearch();
+
+            // Test recursive binary search
+            TestRecursiveBinarySearch();
+        }
+
+        public static void TestIterativeBinarySearch()
+        {
+            int[] array = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
+            int target = 11;
+
+            // stop watch is used to measure time taken for the search
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            int result = BinarySearch.IterativeBinarySearch(array, target);
+            stopwatch.Stop();
+
+            // search target, its index and the search time are printed to console
+            if (result != -1)
+                Console.WriteLine($"Iterative Binary Search: Element {target} found at index {result} in {stopwatch.ElapsedMilliseconds} ms");
+            else
+                Console.WriteLine($"Iterative Binary Search: Element {target} not found");
+        }
+
+        public static void TestRecursiveBinarySearch()
+        {
+            int[] array = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+            int target = 14;
+
+            // stop watch is used to measure time taken for the search
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            int result = BinarySearch.RecursiveBinarySearch(array, target, 0, array.Length - 1);
+            stopwatch.Stop();
+
+            // search target, its index and the search time are printed to console
+            if (result != -1)
+                Console.WriteLine($"Recursive Binary Search: Element {target} found at index {result} in {stopwatch.ElapsedMilliseconds} ms");
+            else
+                Console.WriteLine($"Recursive Binary Search: Element {target} not found");
         }
     }
 }
